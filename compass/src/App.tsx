35b372b7da8fde9fce07a4dfd9340ef1df2744bc
M7md <mohammed2003ha@gmail.com>
2024-06-08 08:49:22 +0400

    some routing done. sill some errors so jus view the code

--- compass/src/App.tsx
+++ compass/src/App.tsx
@@ -2,12 +2,22 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./LandingPage";
 import Matches from "./matches";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import { initializeApp } from "firebase/app";
+import { config } from "./components/config";
+import AuthRoute from "./components/AuthRoute";
+
+initializeApp(config.firebaseConfig);
 
 function App() {
   return (
     <Routes>
       <Route index element={<LandingPage />} />
+      <Route path="/signIn" element={<SignIn />} />
       <Route path="/matches" element={<Matches />} />
+
+      <Route path="/signUp" element={<SignUp />} />
     </Routes>
   );
 }
